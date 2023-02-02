3992230d3ce4d786e7e83deee158446f1310e1bd
Kit <nikitabit@yandex.ru>
2023-02-02 20:30:52 +0700

    Link owners in Flats Delete telephone fields from Flat model

--- property/admin.py
+++ property/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
-# admin.site.register(admin.ModelAdmin)
+
+class OwnersInline(admin.TabularInline):
+    model = Flat.owners.through
+    extra = 1
+    raw_id_fields=["owner"]
+
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
     list_editable = ['new_building']
     list_filter = ['new_building', 'has_balcony', 'rooms_number']
-    raw_id_fields = ['likes']
+    raw_id_fields = ['likes', 'owners']
+    inlines = [
+        OwnersInline,
+    ]
 
 
 @admin.register(Complaint)
--- property/models.py
+++ property/models.py
@@ -6,7 +6,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owners = models.ManyToManyField('Owner', verbose_name="Собственники", blank=True)
+    # owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -52,7 +53,7 @@ class Flat(models.Model):
     new_building = models.BooleanField("Новостройка", null=True)
     likes = models.ManyToManyField(User, blank=True)
 
-    owner_pure_phone = PhoneNumberField("Нормализованный номер владельца", blank=True)
+    # owner_pure_phone = PhoneNumberField("Нормализованный номер владельца", blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
